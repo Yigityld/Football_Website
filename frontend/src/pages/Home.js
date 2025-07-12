d249bde516d8988202831dd999a38e791b87b809
yigit <yigity4002@gmail.com>
2025-07-12 16:49:09 +0300

    .

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -326,7 +326,7 @@ const Home = () => {
           <img
             src="/logo.png"
             alt="Futbol Analiz AI Logo"
-            className="w-25 h-25 rounded-full shadow-lg transition-transform duration-300 hover:rotate-12"
+            className="w-12 h-12 rounded-full shadow-lg transition-transform duration-300 hover:rotate-12"
             style={{ cursor: 'pointer' }}
           />
           <span className="text-xl font-bold text-cyan-200 drop-shadow">Futbol Analiz AI</span>
